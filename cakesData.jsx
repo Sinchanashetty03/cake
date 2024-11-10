7144ccb734d21571aa0eac0c073e94a87f70129b
Sinchanashetty03 <sinchanashetty.ai23@sahyadri.edu.in>
2024-11-10 22:31:02 +0530

    Create cakesData.jsx

---
+++ cakesData.jsx
@@ -0,0 +1,53 @@
+export const cakes = [
+    {
+      id: 1,
+      name: "Chocolate Cake",
+      description: "A rich and moist chocolate cake.",
+      ingredients: "Chocolate, Flour, Sugar, Butter",
+      weight: "1.00 kg",
+      shape: "Round",
+      image: "/assets/full cake.jpg",
+      price: "₨.600"
+    },
+    {
+      id: 2,
+      name: "Strawberry Cake",
+      description: "A fresh and delicious strawberry cake.",
+      ingredients: "Strawberries, Flour, Sugar, Butter",
+      weight: "1.00 kg",
+      shape: "Round",
+      image: "/assets/Strawberry cake.jpg",
+      price: "₨.550"
+    },
+    {
+      id: 3,
+      name: "Vanilla Cake",
+      description: "Classic vanilla cake with cream.",
+      ingredients: "Vanilla, Flour, Sugar, Butter",
+      weight: "1.00 kg",
+      shape: "Square",
+      image: "/assets/vanilla1.avif",
+      price: "₨.500"
+    },
+    {
+      id: 4,
+      name: "Tiramisu Cake",
+      description: "Coffee-flavored Italian dessert cake.",
+      ingredients: "Mascarpone, Coffee, Cocoa Powder",
+      weight: "1.00 kg",
+      shape: "Round",
+      image: "/assets/full tiramisu.jpg",
+      price: "₨.650"
+    },
+    {
+      id: 5,
+      name: "Red Velvet Cake",
+      description: "Soft and creamy red velvet cake.",
+      ingredients: "Cocoa Powder, Flour, Butter, Cream Cheese",
+      weight: "1.00 kg",
+      shape: "Heart",
+      image: "/assets/red full.webp",
+      price: "₨.650"
+    },
+  ];
+  
